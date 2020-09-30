e01609f5dbd985eb92c3156193a145c3d3c3fa01
Anna-fog <anna49.fog@yandex.ru>
2020-09-30 12:45:59 +0300

    About us section added

--- src/js/script.js
+++ src/js/script.js
@@ -1,25 +1,42 @@
 window.addEventListener('DOMContentLoaded', () => {
-    $('.slider__inner').slick({
-        speed: 2200,
-        autoplay: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplaySpeed: 7000,
-        pauseOnHover: false,
-        pauseOnFocus: false,
-        dots: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icn/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icn/right.svg"></button>',
-        // responsive: [
-        //     {
-        //         breakpoint: 992,
-        //         settings: {
-        //             dots: true,
-        //             arrows: false
-        //         }
-        //     }
-        // ]
-    });
+    try {
+        $('.slider__inner').slick({
+            speed: 2200,
+            autoplay: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplaySpeed: 7000,
+            pauseOnHover: false,
+            pauseOnFocus: false,
+            dots: true,
+            prevArrow: '<button type="button" class="slick-prev"><img src="icn/left.svg"></button>',
+            nextArrow: '<button type="button" class="slick-next"><img src="icn/right.svg"></button>',
+            // responsive: [
+            //     {
+            //         breakpoint: 992,
+            //         settings: {
+            //             dots: true,
+            //             arrows: false
+            //         }
+            //     }
+            // ]
+        });
+    } catch (e) {}
+
+
+
+   try {
+       $('.gallery__wrapper').magnificPopup({
+           delegate: 'img',
+           type: 'image',
+           removalDelay: 300,
+           mainClass: 'mfp-fade',
+           gallery:{
+               enabled:true,
+               preload: [1,2],
+           }
+       });
+   } catch (e) {}
 
 
 });
